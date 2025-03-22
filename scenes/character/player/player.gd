c20f8571ca1e80f1c5aab8ddcd9c7d00fbf1bd5e
fylart <fylartthrowaway0@gmail.com>
2025-03-22 12:56:26 +0800

    Fullscreen implemented

--- scenes/character/player/player.gd
+++ scenes/character/player/player.gd
@@ -9,6 +9,7 @@ var player_direction: Vector2
 	
 func _ready() -> void:
 	ToolManager.tool_selected.connect(on_tool_selected)
+	get_viewport().size = DisplayServer.screen_get_size()
 	
 func on_tool_selected(tool: DataTypes.Tools) -> void:
 	current_tool = tool
